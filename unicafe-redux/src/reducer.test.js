e09dd651d999d009c9a82b36ab0ebc6b70fa8392
ezraliberty <ezraliberty99@gmail.com>
2023-11-11 14:23:33 +0100

    implemented Redux

---
+++ unicafe-redux/src/reducer.test.js
@@ -0,0 +1,94 @@
+import deepFreeze from 'deep-freeze'
+import counterReducer from './reducer'
+
+describe('unicafe reducer', () => {
+  const initialState = {
+    good: 0,
+    ok: 0,
+    bad: 0
+  }
+
+  test('should return a proper initial state when called with undefined state', () => {
+    const state = {}
+    const action = {
+      type: 'DO_NOTHING'
+    }
+
+    const newState = counterReducer(undefined, action)
+    expect(newState).toEqual(initialState)
+  })
+
+  test('good is incremented', () => {
+    const action = {
+      type: 'GOOD'
+    }
+    const state = initialState
+
+    deepFreeze(state)
+    const newState = counterReducer(state, action)
+    expect(newState).toEqual({
+      good: 1,
+      ok: 0,
+      bad: 0
+    })
+  })
+
+  test('ok is incremented', () => {
+    const action = {
+      type: 'OK'
+    }
+    const state = initialState
+
+    deepFreeze(state)
+    const newState = counterReducer(state, action)
+    expect(newState).toEqual({
+      good: 0,
+      ok: 1,
+      bad: 0
+    })
+  })
+
+  test('bad is incremented', () => {
+    const action = {
+      type: 'BAD'
+    }
+    const state = initialState
+
+    deepFreeze(state)
+    const newState = counterReducer(state, action)
+    expect(newState).toEqual({
+      good: 0,
+      ok: 0,
+      bad: 1
+    })
+  })
+
+  test('ZERO returns the initialState', () => {
+    const action = {
+      type: 'ZERO'
+    }
+    const state = initialState
+
+    deepFreeze(state)
+    const newState = counterReducer(state, action)
+    expect(newState).toEqual({
+      good: 0,
+      ok: 0,
+      bad: 0
+    })
+  })
+})
+
+/*
+Implement the reducer and its tests.
+
+In the tests, make sure that the reducer is an immutable function with the deep-freeze library. 
+Ensure that the provided first test passes, because Redux expects that the reducer returns a 
+sensible original state when it is called so that the first parameter state, which represents 
+the previous state, is undefined.
+
+Start by expanding the reducer so that both tests pass. Then add the rest of the tests, 
+and finally the functionality that they are testing.
+
+A good model for the reducer is the redux-notes example above.
+*/
