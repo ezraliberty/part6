26eb6c99556ae33223a0772fae8170799f1339e8
ezraliberty <ezraliberty99@gmail.com>
2023-11-15 12:06:29 +0100

    Filter Works

--- redux-anecdotes/src/components/AnecdoteList.jsx
+++ redux-anecdotes/src/components/AnecdoteList.jsx
@@ -2,7 +2,9 @@ import { useSelector, useDispatch } from "react-redux";
 import { voter } from "../reducers/anecdoteReducer";
 
 const AnecdoteList = () => {
-  const anecdotes = useSelector((state) => state);
+  const anecdotes = useSelector(({filter, anecdotes}) => {
+  return filter !== '' ? anecdotes.filter(anecdote =>  anecdote.content.includes(filter)) : anecdotes
+  });
   const dispatch = useDispatch();
 
   const vote = (id) => {
--- redux-anecdotes/src/components/Filter.jsx
+++ redux-anecdotes/src/components/Filter.jsx
@@ -1,6 +1,12 @@
-const Filter = () => {
-    const handleChange = (event) => {
+import { useDispatch } from "react-redux"
+import { filterChange } from "../reducers/filterReducer"
 
+const Filter = () => {
+    const dispatch = useDispatch()
+    // const anecdotes = useSelector(state => state.anecdotes)
+    const handleChange = (event) => {
+        return event.target.value
+    //    return anecdotes.filter(anecdote =>  anecdote.content.includes(event))
     }
 
     const style = {
@@ -9,7 +15,7 @@ const Filter = () => {
 
     return (
         <div style={style}>
-            filter <input onChange={handleChange} />
+            filter <input onChange={() => dispatch(filterChange(handleChange(event)))} />
         </div>
     )
 }
